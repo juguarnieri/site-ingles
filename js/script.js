90bc775023a66dcc0ad00f7be4ed3d5acb654b81
juguarnieri <juliaguarnieri04@gmail.com>
2024-10-23 19:19:11 -0300

    refactor: menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-function toggleSubmenu() {
-    // Seleciona o submenu pelo ID
-    var submenu = document.getElementById("submenu-id");
-    
-    // Verifica o valor atual da propriedade display e alterna entre 'block' e 'none'
-    if (submenu.style.display === "block") {
-        // Se o submenu estiver visível, esconde
-        submenu.style.display = "none";
-    } else {
-        // Se o submenu estiver oculto, exibe
-        submenu.style.display = "block";
-    }
+function toggleMenu() {
+    // A função "toggleMenu" é chamada quando o ícone de menu hamburguer é clicado
+
+    var menu = document.getElementById("matérias-menu");
+    // A função acessa o elemento HTML com o id "matérias-menu", que é o menu que queremos mostrar ou esconder.
+
+    menu.classList.toggle("active");
+    // A classe "active" é adicionada ou removida do menu. Se o menu já tiver a classe "active", ela é removida, o que esconde o menu.
+    // Caso o menu não tenha a classe "active", ela é adicionada, fazendo com que o menu seja exibido.
+    // Isso é útil para alternar entre mostrar e esconder o menu.
+}
+
+function toggleSubmenu(event) {
+    event.preventDefault(); 
+    // Esta linha previne o comportamento padrão do evento de clique no link do submenu.
+    // Sem isso, o clique no link levaria a uma nova página ou rolaria para outra seção da página (dependendo do link).
+
+    var submenu = event.target.nextElementSibling;
+    // A função acessa o elemento seguinte ao elemento que foi clicado (target), ou seja, o submenu que está logo após o link.
+    // Isso funciona porque o submenu está no HTML logo após o link que o ativa.
+
+    submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+    // Esta linha usa um operador ternário para alternar o estilo de exibição do submenu.
+    // Se o valor atual de `submenu.style.display` for "block" (o submenu está visível), ele será alterado para "none", escondendo o submenu.
+    // Se o valor for diferente de "block" (o submenu está escondido ou em outro estado), ele será alterado para "block", mostrando o submenu.
 }
